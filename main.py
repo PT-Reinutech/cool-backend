bae9e54fae23df12ec5ebfb178c3dc4eca92b9ba
Schooze <eatmilky4@gmail.com>
2025-08-13 10:02:23 +0700

    Main, worker for prod

--- main.py
+++ main.py
@@ -278,4 +278,4 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, workers=4)
